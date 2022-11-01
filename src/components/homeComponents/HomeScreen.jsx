24a7eabe87ff83c4697216745fb13e1b389b881b
JRosen09 <2jacobsbusiness@gmail.com>
2022-11-01 11:39:25 -0500

    Food Eatery Project

---
+++ src/components/homeComponents/HomeScreen.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import AdBanner from "./AdBanner";
+import axios from "axios";
+import RecipeCard from "../RecipeCard";
+import { BiSearchAlt2 } from "react-icons/bi";
+import styles from "../RecipeCard.module.css";
+
+const HomeScreen = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  const getRecipes = () => {
+    axios.get("https://recipes.devmountain.com/recipes").then((res) => {
+      setRecipes(res.data);
+      console.log(res.data);
+    });
+  };
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+  console.log(searchInput);
+
+  const recipeDisplay = recipes
+    .filter((recipe) => {
+      let title = recipe.recipe_name.toLowerCase();
+      let searchTerm = searchInput.toLowerCase();
+
+      return title.includes(searchTerm);
+    })
+    .map((recipe) => {
+      return <RecipeCard recipe={recipe} />;
+    });
+
+  return (
+    <div>
+      <AdBanner />
+      <span className={styles.search_bar}>
+        <BiSearchAlt2 size="1.5em" color="#DA7635" />
+        <input
+          placeholder="Search for a Recipe"
+          type="text"
+          onChange={(e) => {
+            setSearchInput(e.target.value);
+          }}
+        />
+      </span>
+      <span className={styles.RecipeDisplay}>{recipeDisplay}</span>
+    </div>
+  );
+};
+
+export default HomeScreen;
